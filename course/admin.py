938c250568984161db453555ac29f2aa720005c1
Andreas Kloeckner <inform@tiker.net>
2014-08-26 12:46:06 -0500

    Fix admin improvement

--- course/admin.py
+++ course/admin.py
@@ -104,10 +104,22 @@ admin.site.register(TimeLabel, TimeLabelAdmin)
 # {{{ participation
 
 class ParticipationAdmin(admin.ModelAdmin):
+    def get_user_first_name(self, obj):
+        return obj.user.first_name
+
+    get_user_first_name.short_description = "First name"
+    get_user_first_name.admin_order_field = "participation__user__first_name"
+
+    def get_user_last_name(self, obj):
+        return obj.user.last_name
+
+    get_user_last_name.short_description = "Last name"
+    get_user_last_name.admin_order_field = "participation__user__last_name"
+
     list_display = (
-            "user__username",
-            "user__first_name",
-            "user__last_name",
+            "user",
+            "user_first_name",
+            "user_last_name",
             "course",
             "role",
             "status",
