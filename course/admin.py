ae7ab81c27e7c03e6c06bc6e68d8189c16b6ef2f
Andreas Kloeckner <inform@tiker.net>
2014-08-28 02:07:36 -0500

    User status admin updates

--- course/admin.py
+++ course/admin.py
@@ -39,7 +39,24 @@ from course.enrollment import (approve_enrollment, deny_enrollment)
 # {{{ user status
 
 class UserStatusAdmin(admin.ModelAdmin):
-    list_display = ("user", "status", "key_time")
+    def get_user_first_name(self, obj):
+        return obj.user.first_name
+
+    get_user_first_name.short_description = "First name"
+    get_user_first_name.admin_order_field = "user__first_name"
+
+    def get_user_last_name(self, obj):
+        return obj.user.last_name
+
+    get_user_last_name.short_description = "Last name"
+    get_user_last_name.admin_order_field = "user__last_name"
+
+    list_display = (
+            "user",
+            "get_user_first_name",
+            "get_user_last_name",
+            "status",
+            "key_time")
     list_filter = ("status",)
 
     date_hierarchy = "key_time"
