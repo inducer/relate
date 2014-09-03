7e8a5d53c87a352fd23030e93bf6f4bf3e58413c
Andreas Kloeckner <inform@tiker.net>
2014-09-03 17:34:41 -0500

    Add missing migration

---
+++ course/migrations/0018_validate_stipulations.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import jsonfield.fields
+import course.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('course', '0017_event_all_day'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='flowaccessexception',
+            name='stipulations',
+            field=jsonfield.fields.JSONField(blank=True, help_text=b'A dictionary of the same things that can be added to a flow access rule, such as allowed_session_count or credit_percent. If not specified here, values will default to the stipulations in the course content.', null=True, validators=[course.models.validate_stipulations]),
+        ),
+    ]
