2951e2f161403fba6122307af3b574a80b0d9db4
Andreas Kloeckner <inform@tiker.net>
2014-09-08 20:56:06 -0500

    Fix update-attempt-ids.py

--- update-attempt-ids.py
+++ update-attempt-ids.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import models, migrations
+import django
+
+django.setup()
+
 from course.models import GradeChange
-
-
-GradeChange = apps.get_model("course", "GradeChange")
 
 for gchange in GradeChange.objects.all():
     if gchange.flow_session is not None:
