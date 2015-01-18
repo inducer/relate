01e50f71865ecbfae2472d7012bbc75af668e56d
Andreas Kloeckner <inform@tiker.net>
2015-01-18 16:45:35 -0600

    Add missing flow rules migration

---
+++ course/migrations/0039_flow_start_rules.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('course', '0038_add_participations_related_name_on_course'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='flowruleexception',
+            name='kind',
+            field=models.CharField(max_length=50, choices=[(b'start', b'Session Start'), (b'access', b'Session Access'), (b'grading', b'Grading')]),
+            preserve_default=True,
+        ),
+    ]
