35f5c62a914bf95c192d5ac43c24e4d6c15df8de
Andreas Kloeckner <inform@tiker.net>
2015-01-25 14:39:25 -0600

    Fix name sorting in participation admin

--- course/admin.py
+++ course/admin.py
@@ -197,13 +197,13 @@ class ParticipationAdmin(admin.ModelAdmin):
         return obj.user.first_name
 
     get_user_first_name.short_description = "First name"
-    get_user_first_name.admin_order_field = "participation__user__first_name"
+    get_user_first_name.admin_order_field = "user__first_name"
 
     def get_user_last_name(self, obj):
         return obj.user.last_name
 
     get_user_last_name.short_description = "Last name"
-    get_user_last_name.admin_order_field = "participation__user__last_name"
+    get_user_last_name.admin_order_field = "user__last_name"
 
     list_display = (
             "user",
