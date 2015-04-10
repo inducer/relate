57bc676c9bf7e7b3d52f3f9be0640522dff81129
Andreas Kloeckner <inform@tiker.net>
2015-04-10 12:13:17 -0500

    Add migration for auditor role

---
+++ course/migrations/0054_add_auditor_role.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('course', '0053_permission_and_help'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='participation',
+            name='role',
+            field=models.CharField(help_text=b'Instructors may update course content. Teaching assistants may access and change grade data. Observers may access analytics. Each role includes privileges from subsequent roles.', max_length=50, choices=[(b'instructor', b'Instructor'), (b'ta', b'Teaching Assistant'), (b'student', b'Student'), (b'observer', b'Observer'), (b'auditor', b'Auditor')]),
+        ),
+        migrations.AlterField(
+            model_name='participationpreapproval',
+            name='role',
+            field=models.CharField(max_length=50, choices=[(b'instructor', b'Instructor'), (b'ta', b'Teaching Assistant'), (b'student', b'Student'), (b'observer', b'Observer'), (b'auditor', b'Auditor')]),
+        ),
+    ]
