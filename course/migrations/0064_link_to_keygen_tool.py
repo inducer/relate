28b20f7ac64ce200b1a42fbbd409270146ba8b78
Andreas Kloeckner <inform@tiker.net>
2015-08-11 16:13:49 -0500

    Add a tool to generate SSH keypairs

---
+++ course/migrations/0064_link_to_keygen_tool.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('course', '0063_tweak_hidden_field_descr'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='course',
+            name='ssh_private_key',
+            field=models.TextField(help_text="An SSH private key to use for Git authentication. Not needed for the sample URL above.You may use <a href='/generate-ssh-key'>this tool</a> to generate a key pair.", verbose_name='SSH private key', blank=True),
+        ),
+    ]
