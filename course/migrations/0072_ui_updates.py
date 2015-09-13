cca085307648686790f82c053b929d3f24973dba
Andreas Kloeckner <inform@tiker.net>
2015-09-13 00:36:44 -0500

    Migration for UI changes

---
+++ course/migrations/0072_ui_updates.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('course', '0071_add_issue_ticket_permission'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='course',
+            name='course_root_path',
+            field=models.CharField(help_text='Subdirectory *within* the git repository to use as course root directory. Not required, and usually blank. Use only if your course content lives in a subdirectory of your git repository. Should not include trailing slash.', max_length=200, verbose_name='Course root in repository', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='examticket',
+            name='usage_time',
+            field=models.DateTimeField(null=True, verbose_name='Date and time of first usage of ticket', blank=True),
+        ),
+    ]
