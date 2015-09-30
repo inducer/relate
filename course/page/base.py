849f9a35300c2397103e7d921b3aabafc227e9c8
Andreas Kloeckner <inform@tiker.net>
2015-09-29 21:21:49 -0500

    Include a link in grade notification emails

--- course/page/base.py
+++ course/page/base.py
@@ -56,17 +56,20 @@ class PageContext(object):
 
         May be None.
 
+    .. attribute:: page_uri
+
     Note that this is different from :class:`course.utils.FlowPageContext`,
     which is used internally by the flow views.
     """
 
     def __init__(self, course, repo, commit_sha, flow_session,
-            in_sandbox=False):
+            in_sandbox=False, page_uri=None):
         self.course = course
         self.repo = repo
         self.commit_sha = commit_sha
         self.flow_session = flow_session
         self.in_sandbox = in_sandbox
+        self.page_uri = page_uri
 
 
 class PageBehavior(object):
@@ -734,6 +737,7 @@ class PageBaseWithHumanTextFeedback(PageBase):
                     "participation": page_context.flow_session.participation,
                     "feedback_text": grade_data["feedback_text"],
                     "flow_session": page_context.flow_session,
+                    "review_uri": page_context.page_uri,
                     })
 
                 from django.core.mail import EmailMessage
