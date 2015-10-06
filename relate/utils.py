d7c780c19cc80f4c1f81779e9653b4e9e83bcf55
Andreas Kloeckner <inform@tiker.net>
2015-10-06 13:48:26 -0500

    Add a facility for hang debuggin

--- relate/utils.py
+++ relate/utils.py
@@ -168,4 +168,30 @@ def retry_transaction_decorator(f, max_tries=None):
     return wrapper
 
 
+# {{{ hang debugging
+
+def dumpstacks(signal, frame):
+    import threading
+    import sys
+    import traceback
+
+    id2name = dict([(th.ident, th.name) for th in threading.enumerate()])
+    code = []
+    for threadId, stack in sys._current_frames().items():
+        code.append("\n# Thread: %s(%d)" % (id2name.get(threadId, ""), threadId))
+        for filename, lineno, name, line in traceback.extract_stack(stack):
+            code.append('File: "%s", line %d, in %s' % (filename, lineno, name))
+            if line:
+                code.append("  %s" % (line.strip()))
+    print "\n".join(code)
+
+if 0:
+    import signal
+    import os
+    print("*** HANG DUMP HANDLER ACTIVATED: 'kill -USR1 %s' to dump stacks"
+            % os.getpid())
+    signal.signal(signal.SIGUSR1, dumpstacks)
+
+# }}}
+
 # vim: foldmethod=marker
