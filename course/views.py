872ff820f5e55234b2568036410f80c3ee025313
Andreas Kloeckner <inform@tiker.net>
2015-10-09 16:49:42 -0500

    Exceptions markup fix

--- course/views.py
+++ course/views.py
@@ -731,9 +731,10 @@ def grant_exception_stage_2(pctx, participation_id, flow_id):
     create_session_is_override = False
     if not session_start_rule.may_start_new_session:
         create_session_is_override = True
-        form_text += ("<div class='alert alert-info'>%s</div>"
-                "<i class='fa fa-info-circle'></i>" %
-                (_("Creating a new session is (technically) not allowed "
+        form_text += ("<div class='alert alert-info'>%s</div>" % (
+                "<i class='fa fa-info-circle'></i>"
+                +
+                _("Creating a new session is (technically) not allowed "
                 "by course rules. Clicking 'Create Session' anyway will "
                 "override this rule.")))
 
