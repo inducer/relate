7bf081a25fd761f5f659844e1a99374c0250a888
dzhuang <dzhuang.scut@gmail.com>
2016-01-13 16:32:05 +0800

    update i18n tags and zh_CN translation. Fix markdown in model help_text.

--- course/models.py
+++ course/models.py
@@ -62,7 +62,7 @@ class Course(models.Model):
             help_text=_("A course identifier. Alphanumeric with dashes, "
             "no spaces. This is visible in URLs and determines the location "
             "on your file system where the course's git repository lives. "
-            "This should *not* be changed after the course has been created "
+            "This should <em>not</em> be changed after the course has been created "
             "without also moving the course's git on the server."),
             verbose_name=_('Course identifier'),
             db_index=True,
@@ -77,16 +77,19 @@ class Course(models.Model):
     name = models.CharField(
             null=True, blank=False,
             max_length=200,
+            verbose_name=_('Course name'),
             help_text=_("A human-readable name for the course. "
                 "(e.g. 'Numerical Methods')"))
     number = models.CharField(
             null=True, blank=False,
             max_length=200,
+            verbose_name=_('Course number'),
             help_text=_("A human-readable course number/ID "
                 "for the course (e.g. 'CS123')"))
     time_period = models.CharField(
             null=True, blank=False,
             max_length=200,
+            verbose_name=_('Time Period'),
             help_text=_("A human-readable description of the "
                 "time period for the course (e.g. 'Fall 2014')"))
 
@@ -123,7 +126,7 @@ class Course(models.Model):
             verbose_name=_('SSH private key'))
     course_root_path = models.CharField(max_length=200, blank=True,
             help_text=_(
-                'Subdirectory *within* the git repository to use as '
+                'Subdirectory <em>within</em> the git repository to use as '
                 'course root directory. Not required, and usually blank. '
                 'Use only if your course content lives in a subdirectory '
                 'of your git repository. '
