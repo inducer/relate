fee1afc9faf7481d92f4ae8d923c864021a27164
dzhuang <dzhuang.scut@gmail.com>
2016-01-19 18:42:59 +0800

    Allowing participation preapproval by institutional ID.

---
+++ course/migrations/0082_alter_models_for_preapproval_by_inst_id.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('course', '0081_course_fields_i18n'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='course',
+            name='preapproval_require_verified_inst_id',
+            field=models.BooleanField(default=True, help_text='If set, students cannot get particiaption preapproval using institutional ID if institutional ID they provided are not verified.', verbose_name='None preapproval by institutional ID if not verified?'),
+        ),
+        migrations.AddField(
+            model_name='participationpreapproval',
+            name='institutional_id',
+            field=models.CharField(max_length=254, null=True, verbose_name='Institutional ID', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='participationpreapproval',
+            name='email',
+            field=models.EmailField(max_length=254, null=True, verbose_name='Email', blank=True),
+        ),
+    ]
