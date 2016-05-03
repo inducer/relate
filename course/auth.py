044efec1e11040364aa2db56cdf7d65a03169b4c
Andreas Kloeckner <inform@tiker.net>
2016-05-03 00:05:11 -0500

    SAML2 logout tweak

--- course/auth.py
+++ course/auth.py
@@ -947,12 +947,13 @@ class Saml2Backend(Saml2BackendBase):
 
 @never_cache
 def sign_out(request):
+    auth_logout(request)
+
     if settings.RELATE_SIGN_IN_BY_SAML2_ENABLED:
         from djangosaml2.views import _get_subject_id, logout as saml2_logout
         if _get_subject_id(request.session) is not None:
             return saml2_logout(request)
 
-    auth_logout(request)
     return redirect("relate-home")
 
 # }}}
