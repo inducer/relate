3ddbcf7fc08bd6af5c4c663d9f3c0d9eee0c5e2e
Andreas Kloeckner <inform@tiker.net>
2016-09-23 10:04:57 -0500

    Course creation: Keep 'repo' variable for error path

--- course/versioning.py
+++ course/versioning.py
@@ -269,7 +269,6 @@ def set_up_new_course(request):
                                 vrepo, new_course.course_file,
                                 new_course.events_file, new_sha)
 
-                        del repo
                         del vrepo
 
                         new_course.active_git_commit_sha = new_sha.decode()
