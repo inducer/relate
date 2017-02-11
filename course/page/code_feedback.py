cd85a7beb8696133766217e6c39e526040c35dcd
Andreas Kloeckner <inform@tiker.net>
2017-02-11 12:38:34 -0600

    Add complex support to check_scalar

--- course/page/code_feedback.py
+++ course/page/code_feedback.py
@@ -136,7 +136,7 @@ class Feedback:
             rtol=1e-5, atol=1e-8, report_success=True, report_failure=True):
         import numpy as np
 
-        if not isinstance(data, (float, int, np.number)):
+        if not isinstance(data, (complex, float, int, np.number)):
             try:
                 # Check whether data is a sympy number because sympy
                 # numbers do not follow the typical interface
