15ab7f656f739203ff4f0c4413eb9dee27a85205
Andreas Kloeckner <inform@tiker.net>
2018-02-23 00:49:01 -0600

    Fix run-travis-ci.sh script

--- run-travis-ci.sh
+++ run-travis-ci.sh
@@ -12,8 +12,8 @@ fi
 # run ci according to env variables
 if [[ $RL_TRAVIS_TEST == test ]]; then
   . ./run-tests-for-ci.sh
-if [[ $RL_TRAVIS_TEST == cmdline ]]; then
-  . ./test-command-line-tool.sh
+elif [[ $RL_TRAVIS_TEST == cmdline ]]; then
+  . ./test-command-line-tool.sh python3.6
 elif [[ $RL_TRAVIS_TEST == mypy ]]; then
   . ./prepare-and-run-mypy.sh python3.6 mypy==0.560
 elif [[ $RL_TRAVIS_TEST == flake8 ]]; then
