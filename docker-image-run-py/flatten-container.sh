ce0c7692be051ef83ea73fe24b0f77f9eeceae22
Andreas Kloeckner <inform@tiker.net>
2018-10-14 20:19:41 -0500

    Fix container flattening script

--- docker-image-run-py/flatten-container.sh
+++ docker-image-run-py/flatten-container.sh
@@ -1,13 +1,12 @@
-#! /usr/bin/bash
+#! /bin/bash
 
 if test "$1" = ""; then 
   echo "$0 imagename"
   exit 1
 fi
 CONTAINER=$(docker create "$1")
-docker export "CONTAINER" | \
+docker export "$CONTAINER" | \
   docker import \
-    -c "MAINTAINER Andreas Kloeckner <inform@tiker.net>" \
     -c "EXPOSE 9941" \
     -
 docker rm -f $CONTAINER
