ecec9aeb2a61bae297dd90e16374546c0b0ab4bc
Andreas Kloeckner <inform@tiker.net>
2018-10-14 22:17:39 -0500

    Fix another daylight savings fail in calendar tests

--- tests/test_calendar.py
+++ tests/test_calendar.py
@@ -142,7 +142,14 @@ class CreateRecurringEventsTest(SingleCourseTestMixin,
                 t = evt.time
                 continue
             else:
-                self.assertEqual(evt.time - t, datetime.timedelta(weeks=1))
+                self.assertTrue(
+                        evt.time - t
+                        >=
+                        (datetime.timedelta(weeks=1) - datetime.timedelta(hours=1)))
+                self.assertTrue(
+                        evt.time - t
+                        <=
+                        (datetime.timedelta(weeks=1) + datetime.timedelta(hours=1)))
                 t = evt.time
 
     def test_post_success_starting_ordinal_not_specified(self):
