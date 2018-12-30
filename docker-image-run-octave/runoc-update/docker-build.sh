0e8768dca313939e592bc694afbd705d1e383dd3
Neal Davis <davis68@illinois.edu>
2018-12-30 16:06:10 -0600

    Flatten Docker image back into one.

--- docker-image-run-octave/runoc-update/docker-build.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-cp ../../course/page/code_feedback.py ../../course/page/code_runoc_backend.py .
-docker build --no-cache . -t inducer/runoc-update
-rm code_feedback.py code_runoc_backend.py
