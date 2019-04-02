6f8980c76f46173b21833c119425874fddf0e1a7
Neal Davis <davis68@illinois.edu>
2019-04-02 16:55:58 -0500

    First stab at a CodeQuestion interface.  UNTESTED but pushed for backup

--- docker-image-run-octave/docker-build.sh
+++ docker-image-run-octave/docker-build.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
-cp ../course/page/code_feedback.py ../course/page/code_runoc_backend.py .
+cp ../course/page/code_feedback.py .
+cp ../course/page/code_runoc_backend.py code_run_backend.py
 docker build --no-cache . -t inducer/runoc
-rm code_feedback.py code_runoc_backend.py
+rm code_feedback.py code_run_backend.py
--- docker-image-run-py/docker-build.sh
+++ docker-image-run-py/docker-build.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-cp ../course/page/code_feedback.py ../course/page/code_runpy_backend.py .
+cp ../course/page/code_feedback.py .
+cp ../course/page/code_runpy_backend.py code_run_backend.py
 docker build --no-cache . -t inducer/relate-runpy-amd64
-rm code_feedback.py code_runpy_backend.py
-
+rm code_feedback.py code_run_backend.py
