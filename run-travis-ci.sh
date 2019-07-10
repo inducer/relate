f7d27fea8d8c1580876b48417f00e76ef27a31ea
Andreas Kloeckner <inform@tiker.net>
2019-07-10 16:36:24 -0500

    Fix run-travis-ci.sh to respect PY_EXE

--- run-travis-ci.sh
+++ run-travis-ci.sh
@@ -13,9 +13,9 @@ fi
 if [[ $RL_TRAVIS_TEST == test* ]]; then
   . ./run-tests-for-ci.sh
 elif [[ $RL_TRAVIS_TEST == cmdline ]]; then
-  . ./test-command-line-tool.sh python3.6
+  . ./test-command-line-tool.sh $PY_EXE
 elif [[ $RL_TRAVIS_TEST == mypy ]]; then
-  . ./prepare-and-run-mypy.sh python3.6 mypy==0.641
+  . ./prepare-and-run-mypy.sh $PY_EXE mypy==0.641
 elif [[ $RL_TRAVIS_TEST == flake8 ]]; then
   . ./prepare-and-run-flake8.sh relate course accounts tests bin
 fi
