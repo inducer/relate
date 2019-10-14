909f9ba59ec17cbf2397c2214dc52d8b50a45a39
Neal Davis <davis68@illinois.edu>
2019-10-14 16:45:10 -0500

    Add basic Docker stub.

---
+++ docker-image-run-octave/docker-build.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+cp ../course/page/code_feedback.py .
+cp ../course/page/code_runoctave_backend.py code_run_backend.py
+docker build --no-cache . -t davis68/relate-runcode-octave
+rm code_feedback.py code_run_backend.py
