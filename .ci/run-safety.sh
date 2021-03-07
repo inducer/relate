89f1b1ff9a7417f6a5b1322caeeec0ff00f34d87
Andreas Kloeckner <inform@tiker.net>
2021-03-07 14:31:15 -0600

    Address, ignore safety-39535

--- .ci/run-safety.sh
+++ .ci/run-safety.sh
@@ -2,8 +2,12 @@
 # 38678: django-celery-results: no update currently available
 # https://github.com/celery/django-celery-results/issues/154
 # 39253:  py.path.svnwc DOS
+# 39535:
+#   https://github.com/inducer/relate/pull/779
+#   amounts to pysaml2 >= 6.5.1 version bump, done in pyproject.toml
 # code not used
 poetry run safety check \
         -i 38678 \
         -i 39253 \
+        -i 39535 \
         --full-report
