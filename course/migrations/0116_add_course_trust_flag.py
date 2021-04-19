878cace51bd8e44f4e36c1907ae8419c5317f635
Andreas Kloeckner <inform@tiker.net>
2021-04-18 19:05:46 -0500

    Grandfather in existing courses on trusted_for_markup

--- course/migrations/0116_add_course_trust_flag.py
+++ course/migrations/0116_add_course_trust_flag.py
@@ -3,16 +3,28 @@
 from django.db import migrations, models
 
 
+def trust_existing_courses_for_markup(apps, schema_editor):
+    Course = apps.get_model("course", "Course")  # noqa: N806
+    for course in Course.objects.all():
+        # Existing courses are grandfathered in.
+        course.trusted_for_markup = True
+        course.save()
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('course', '0115_catch_up'),
+        ("course", "0115_catch_up"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='course',
-            name='trusted_for_markup',
-            field=models.BooleanField(default=False, verbose_name='May present arbitrary HTML to course participants'),
+            model_name="course",
+            name="trusted_for_markup",
+            field=models.BooleanField(
+                default=False,
+                verbose_name="May present arbitrary HTML to course participants",
+            ),
         ),
+        migrations.RunPython(trust_existing_courses_for_markup),
     ]
