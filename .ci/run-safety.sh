13cf505fc3cba39e8ee54d813b0dfa4b5687100d
Andreas Klöckner <inform@tiker.net>
2021-05-01 23:24:44 -0500

    Add ignore for safety warning 40291 (pip)

--- .ci/run-safety.sh
+++ .ci/run-safety.sh
@@ -5,9 +5,11 @@
 # 39535:
 #   https://github.com/inducer/relate/pull/779
 #   amounts to pysaml2 >= 6.5.1 version bump, done in pyproject.toml
+# 40291: affects pip, not related to relate's safety
 # code not used
 poetry run safety check \
         -i 38678 \
         -i 39253 \
         -i 39535 \
+        -i 40291 \
         --full-report
