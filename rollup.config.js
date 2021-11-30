245e7bfcaa1cc39e462344742f0513557fff6b66
Andreas Kloeckner <inform@tiker.net>
2021-11-29 23:22:56 -0600

    Compress bundles for more efficient serving

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,27 @@
 import resolve from '@rollup/plugin-node-resolve';
+import { brotliCompress } from 'zlib';
+import { promisify } from 'util';
 import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 import styles from 'rollup-plugin-styles';
+import gzipPlugin from 'rollup-plugin-gzip';
 
 // `npm run build` -> `production` is true
 // `npm run dev` -> `production` is false
 const production = !process.env.ROLLUP_WATCH;
+
+const brotliPromise = promisify(brotliCompress);
 
 const defaultPlugins = [
   resolve(),
   styles(),
   commonjs(),
   production && terser(), // minify, but only in production
+  production && gzipPlugin(),
+  production && gzipPlugin({
+    customCompression: (content) => brotliPromise(Buffer.from(content)),
+    fileName: '.br',
+  }),
 ];
 
 export default [
