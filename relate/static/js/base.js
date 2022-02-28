6cf7cbaa7120f6d06378e8f3dc63c7626fcc5366
Andreas Kloeckner <inform@tiker.net>
2022-02-27 19:44:28 -0600

    Drop jQuery UI

--- relate/static/js/base.js
+++ relate/static/js/base.js
@@ -3,8 +3,6 @@ import jQuery from 'jquery';
 import 'bootstrap';
 
 import select2 from 'select2';
-import 'jquery-ui-dist/jquery-ui';
-
 import * as rlUtils from './rlUtils';
 
 import '../css/base.scss';
