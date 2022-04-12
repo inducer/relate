158b86ae4c0b913c12694173616ffa992fb0aa2b
Neal Davis <davis68@illinois.edu>
2022-04-12 10:52:45 -0500

    Create docker-build.sh

---
+++ docker-image-run-octave/docker-build.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+cp ../course/page/code_feedback.py .
+cp ../course/page/code_run_backend_octave.py ./code_run_backend.py
+docker build --no-cache . -t davis68/relate-octave
+rm code_feedback.py code_run_backend.py
