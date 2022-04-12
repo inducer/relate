bc25a9429e0523ec106018dff9cf6b4f266fc76b
Neal Davis <davis68@illinois.edu>
2022-04-12 10:54:11 -0500

    Update docker-build.sh

--- docker-image-run-py/docker-build.sh
+++ docker-image-run-py/docker-build.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 cp ../course/page/code_feedback.py .
-cp ../course/page/code_run_backend.py .
+cp ../course/page/code_run_backend_py.py ./code_run_backend.py
 docker build --no-cache . -t inducer/relate-runcode-python
 rm code_feedback.py code_run_backend.py
