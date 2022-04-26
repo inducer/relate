c51303023d208164bc17c2e2533d2ad427562089
Andreas Kloeckner <inform@tiker.net>
2022-04-26 18:44:42 -0500

    Auto-show uploaded PDF
    Dial up height of uploaded PDF preview
    
    Fix phrasing of uploaded file review link

--- relate/static/js/rlUtils.js
+++ relate/static/js/rlUtils.js
@@ -16,3 +16,63 @@ export function getCookie(name) {
   }
   return cookieValue;
 }
+
+// {{{ file upload support
+
+function embedUploadedFileViewer(mimeType, dataUrl) {
+  jQuery('#file_upload_viewer_div').html(
+    `<object data='${dataUrl}' type='${mimeType}' style='width:100%; height: 80vh;'>
+    <p>(
+    Your browser reported itself unable to render <tt>${dataUrl.mimeType}</tt> inline.
+    )</p>
+    </object>`,
+  );
+}
+
+function matchUploadDataURL(dataUrl) {
+  // take apart data URL
+  const parts = dataUrl.match(/data:([^;]*)(;base64)?,([0-9A-Za-z+/]+)/);
+  return {
+    mimeType: parts[1],
+    encoding: parts[2],
+    base64Data: parts[3],
+  };
+}
+
+function convertUploadDataUrlToObjectUrl(dataUrl) {
+  // https://code.google.com/p/chromium/issues/detail?id=69227#37
+  const isWebKit = /WebKit/.test(navigator.userAgent);
+
+  if (isWebKit) {
+    // assume base64 encoding
+    const binStr = atob(dataUrl.base64Data);
+
+    // convert to binary in ArrayBuffer
+    const buf = new ArrayBuffer(binStr.length);
+    const view = new Uint8Array(buf);
+    for (let i = 0; i < view.length; i += 1) {
+      view[i] = binStr.charCodeAt(i);
+    }
+
+    const blob = new Blob([view], { type: dataUrl.mimeType });
+    return webkitURL.createObjectURL(blob); // eslint-disable-line no-undef
+  }
+  return null;
+}
+
+export function enablePreviewForFileUpload() {
+  const dataUrl = matchUploadDataURL(jQuery('#file_upload_download_link').attr('href'));
+  const objUrl = convertUploadDataUrlToObjectUrl(dataUrl);
+
+  if (objUrl) {
+    jQuery('#file_upload_download_link').attr('href', objUrl);
+  }
+
+  if (dataUrl.mimeType === 'application/pdf') {
+    embedUploadedFileViewer(dataUrl.mimeType, objUrl);
+  }
+}
+
+// }}}
+
+// vim: foldmethod=marker
