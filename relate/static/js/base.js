5a473da4242483604b4d3373a3d47befa6d49d15
Andreas Kloeckner <inform@tiker.net>
2022-04-27 01:04:01 -0500

    Allow automatically computing points from feedback

--- relate/static/js/base.js
+++ relate/static/js/base.js
@@ -1,6 +1,6 @@
 import './jquery-importer';
 import jQuery from 'jquery';
-import 'bootstrap';
+import * as bootstrap from 'bootstrap';
 
 import select2 from 'select2';
 import * as rlUtils from './rlUtils';
@@ -10,4 +10,4 @@ import '../css/base.scss';
 select2(jQuery);
 
 /* eslint-disable-next-line import/prefer-default-export */
-export { rlUtils };
+export { rlUtils, bootstrap };
