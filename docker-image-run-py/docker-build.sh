a610e9c2f9e69ee607316b168c0c068edde9400d
Andreas Kloeckner <inform@tiker.net>
2022-08-24 13:17:12 -0500

    Add -f option to docker-build.sh for UIUC image

--- docker-image-run-py/docker-build.sh
+++ docker-image-run-py/docker-build.sh
@@ -1,5 +1,15 @@
 #! /bin/sh
+
+if test "$1" = "-f"; then
+        RLCONTAINER=full
+        IMGNAME=inducer/relate-runcode-python-amd64-tf-opencl-loopy
+else
+        RLCONTAINER=base
+        IMGNAME=inducer/relate-runcode-python-amd64
+fi
+
 cp ../course/page/code_feedback.py .
 cp ../course/page/code_run_backend.py .
-docker build --no-cache . -t inducer/relate-runcode-python
+
+docker build --no-cache --build-arg RLCONTAINER="$RLCONTAINER" . -t $IMGNAME
 rm code_feedback.py code_run_backend.py
