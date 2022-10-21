ad88e26def7077a4b37e7fdfe53f54e7c4908aba
Andreas Kloeckner <inform@tiker.net>
2022-10-20 23:44:51 -0500

    Fix exercise-docker script for updated request_run

--- exercise-docker.py
+++ exercise-docker.py
@@ -1,5 +1,5 @@
 def main():
-    from course.page import request_python_run
+    from course.page.code import request_run
 
     req = {
         "setup_code": "a,b=5,2",
@@ -13,7 +13,7 @@ def main():
     while True:
         print(count)
         count += 1
-        res = request_python_run(req, 5, image="inducer/runpy-i386")
+        res = request_run(req, 10)
         if res["result"] != "success":
             print(res)
             break
