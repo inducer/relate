8bfac9c67c4d34cdccbbe25ca0485124eae702cd
Andreas Kloeckner <inform@tiker.net>
2023-09-20 12:07:02 -0500

    Jupyterlite: add exam mode build

--- contrib/jupyterlite/build.sh
+++ contrib/jupyterlite/build.sh
@@ -2,6 +2,12 @@
 
 set -eo pipefail
 
+EXAM=0
+if test "$1" == "--exam"; then
+    echo "BUILDING IN EXAM MODE"
+    EXAM=1
+fi
+
 MATHJAX_VER=2.7.7
 
 rm -Rf env _output files pack
@@ -13,15 +19,18 @@ source env/bin/activate
 pip install jupyterlite-core jupyterlite-pyodide-kernel jupyter-server libarchive-c
 
 mkdir -p pack
-mkdir -p files/{cs450,cs555}
 
-git clone https://github.com/inducer/numerics-notes pack/numerics-notes
-git clone https://github.com/inducer/numpde-notes pack/numpde-notes
+if [[ "$EXAM" = 0 ]]; then
+    mkdir -p files/{cs450,cs555}
 
-cp -R pack/numerics-notes/demos files/cs450/demos
-cp -R pack/numerics-notes/cleared-demos files/cs450/cleared
-cp -R pack/numpde-notes/demos files/cs555/demos
-cp -R pack/numpde-notes/cleared-demos files/cs555/cleared
+    git clone https://github.com/inducer/numerics-notes pack/numerics-notes
+    git clone https://github.com/inducer/numpde-notes pack/numpde-notes
+
+    cp -R pack/numerics-notes/demos files/cs450/demos
+    cp -R pack/numerics-notes/cleared-demos files/cs450/cleared
+    cp -R pack/numpde-notes/demos files/cs555/demos
+    cp -R pack/numpde-notes/cleared-demos files/cs555/cleared
+fi
 
 curl -L "https://github.com/mathjax/MathJax/archive/$MATHJAX_VER.zip" \
         -o "pack/mathjax-$MATHJAX_VER.zip"
--- contrib/jupyterlite/upload.sh
+++ contrib/jupyterlite/upload.sh
@@ -1,2 +1,7 @@
 #! /bin/bash
-rsync --archive --delete -v _output/ rl:jupyterlite/
+
+if test -d files/cs450; then
+    rsync --archive --delete -v _output/ rl:jupyterlite/
+else
+    rsync --archive --delete -v _output/ rl:jupyterlite-exam/
+fi
