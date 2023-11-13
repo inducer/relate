3386ea167288a0202dca3b9aff9b83276f2e0db5
Andreas Kloeckner <inform@tiker.net>
2023-11-13 12:46:41 -0600

    Jupyterlite upload: provide feedback on where upload is going

--- contrib/jupyterlite/upload.sh
+++ contrib/jupyterlite/upload.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
 if test -d files/cs450; then
+    echo "UPLOADING FOR DEFAULT MODE"
     rsync --archive --delete -v _output/ rl:/web/jupyterlite/main/
 else
+    echo "UPLOADING FOR EXAM MODE"
     rsync --archive --delete -v _output/ rl:/web/jupyterlite/exam/
 fi
