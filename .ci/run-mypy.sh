46bd49a7511c1b18fe55018a0940a2921a8c34c7
Andreas Kloeckner <inform@tiker.net>
2024-07-11 10:51:44 -0500

    Migrate mypy config to pyproject.toml, improve

--- .ci/run-mypy.sh
+++ .ci/run-mypy.sh
@@ -1,10 +1,3 @@
 #! /bin/bash
 
-mypy \
-  --strict-optional \
-  --show-error-codes \
-  --ignore-missing-imports \
-  --follow-imports=skip \
-  --disallow-untyped-calls \
-  relate course
-  # --disallow-untyped-defs \
+mypy relate course accounts
