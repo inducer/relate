700c08ef80cb47b9af1987b984b15626a440161d
Andreas Kloeckner <inform@tiker.net>
2024-07-25 14:21:20 -0500

    Datatables bundle: set to non-strict

--- rollup.config.js
+++ rollup.config.js
@@ -69,6 +69,9 @@ export default [
       format: 'iife',
       sourcemap: true,
       name: 'rlDataTables',
+      // Otherwise there are complaints about datatables trying to set attributes
+      // on window.
+      strict: false,
     },
     plugins: defaultPlugins,
   },
