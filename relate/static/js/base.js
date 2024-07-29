a82f2dd08ace5d49ee69db0bc667c67c9c530274
Andreas Kloeckner <inform@tiker.net>
2024-07-29 13:29:40 -0500

    Offer Bootstrap toast as a global utility function

--- relate/static/js/base.js
+++ relate/static/js/base.js
@@ -4,10 +4,12 @@ import * as bootstrap from 'bootstrap';
 
 import select2 from 'select2';
 import * as rlUtils from './rlUtils';
+import * as bsUtils from './bsUtils';
 
 import '../css/base.scss';
 
 select2(jQuery);
 
 globalThis.rlUtils = rlUtils;
+globalThis.bsUtils = bsUtils;
 globalThis.bootstrap = bootstrap;
