inducer/relate

commit ea36dc0dab3e2ed568f403c6af4e97fde7b0f363

Upgrade to codemirror 6

course/page/code.py
@@ -155,7 +155,6 @@ class CodeForm(StyledForm):
         cm_widget, cm_help_text = get_codemirror_widget(
                 language_mode=language_mode,
                 interaction_mode=interaction_mode,
-                read_only=read_only,
 
                 # Automatically focus the text field once there has
                 # been some input.
@@ -167,9 +166,8 @@ class CodeForm(StyledForm):
             initial=initial_code,
             help_text=cm_help_text,
             widget=cm_widget,
+            disabled=read_only,
             label=_("Answer"))
-
-        self.style_codemirror_widget()
 
     def clean(self):
         # FIXME Should try compilation

course/page/text.py
@@ -49,7 +49,7 @@ class TextAnswerForm(StyledForm):
     use_required_attribute = False
 
     @staticmethod
-    def get_text_widget(widget_type, read_only=False, check_only=False,
+    def get_text_widget(widget_type, check_only=False,
             interaction_mode=None, initial_text=None):
         """Returns None if no widget found."""
 
@@ -59,8 +59,6 @@ class TextAnswerForm(StyledForm):
 
             widget = forms.TextInput()
             widget.attrs["autofocus"] = None
-            if read_only:
-                widget.attrs["readonly"] = None
             return widget, None
 
         elif widget_type == "textarea":
@@ -68,9 +66,7 @@ class TextAnswerForm(StyledForm):
                 return True
 
             widget = forms.Textarea()
-            # widget.attrs["autofocus"] = None
-            if read_only:
-                widget.attrs["readonly"] = None
+            widget.attrs["autofocus"] = None
             return widget, None
 
         elif widget_type.startswith("editor:"):
@@ -80,8 +76,7 @@ class TextAnswerForm(StyledForm):
             from course.utils import get_codemirror_widget
             cm_widget, cm_help_text = get_codemirror_widget(
                     language_mode=widget_type[widget_type.find(":")+1:],
-                    interaction_mode=interaction_mode,
-                    read_only=read_only)
+                    interaction_mode=interaction_mode)
 
             return cm_widget, cm_help_text
 
@@ -94,17 +89,16 @@ class TextAnswerForm(StyledForm):
 
         super().__init__(*args, **kwargs)
         widget, help_text = self.get_text_widget(
-                    widget_type, read_only,
+                    widget_type,
                     interaction_mode=interaction_mode)
         self.validators = validators
         self.fields["answer"] = forms.CharField(
                 required=True,
                 initial=initial_text,
+                disabled=read_only,
                 widget=widget,
                 help_text=help_text,
                 label=_("Answer"))
-
-        self.style_codemirror_widget()
 
     def clean(self):
         cleaned_data = super().clean()

course/utils.py
@@ -25,11 +25,15 @@ THE SOFTWARE.
 
 import datetime
 from contextlib import ContextDecorator
+from dataclasses import dataclass
 from typing import (
-    TYPE_CHECKING, Any, Iterable, cast,
+    TYPE_CHECKING,
+    Any,
+    Iterable,
+    cast,
 )
 
-from django import http
+from django import forms, http
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import get_object_or_404, render
 from django.utils import translation
@@ -37,9 +41,16 @@ from django.utils.translation import gettext as _, pgettext_lazy
 
 from course.constants import flow_permission, flow_rule_kind
 from course.content import (
-    CourseCommitSHADoesNotExist, FlowDesc, FlowPageDesc, FlowSessionAccessRuleDesc,
-    FlowSessionGradingRuleDesc, FlowSessionStartRuleDesc, get_course_commit_sha,
-    get_course_repo, get_flow_desc, parse_date_spec,
+    CourseCommitSHADoesNotExist,
+    FlowDesc,
+    FlowPageDesc,
+    FlowSessionAccessRuleDesc,
+    FlowSessionGradingRuleDesc,
+    FlowSessionStartRuleDesc,
+    get_course_commit_sha,
+    get_course_repo,
+    get_flow_desc,
+    parse_date_spec,
 )
 from course.page.base import PageBase, PageContext
 from relate.utils import string_concat
@@ -48,11 +59,13 @@ from relate.utils import string_concat
 # {{{ mypy
 
 if TYPE_CHECKING:
-    from codemirror import CodeMirrorJavascript, CodeMirrorTextarea  # noqa
-
     from course.content import Repo_ish
     from course.models import (
-        Course, ExamTicket, FlowPageData, FlowSession, Participation,
+        Course,
+        ExamTicket,
+        FlowPageData,
+        FlowSession,
+        Participation,
     )
     from relate.utils import Repo_ish  # noqa
 
@@ -880,110 +893,116 @@ class PageInstanceCache:
 
 # {{{ codemirror config
 
+@dataclass(frozen=True)
+class JsLiteral:
+    js: str
+
+
+def repr_js(obj: Any) -> str:
+    if isinstance(obj, list):
+        return "[%s]" % ", ".join(repr_js(ch) for ch in obj)
+    elif isinstance(obj, dict):
+        return "{%s}" % ", ".join(f"{k}: {repr_js(v)}" for k, v in obj.items())
+    elif isinstance(obj, bool):
+        return repr(obj).lower()
+    elif isinstance(obj, (int, float)):
+        return repr(obj)
+    elif isinstance(obj, str):
+        return repr(obj)
+    elif isinstance(obj, JsLiteral):
+        return obj.js
+    else:
+        raise ValueError(f"unsupported object type: {type(obj)}")
+
+
+class CodeMirrorTextarea(forms.Textarea):
+    @property
+    def media(self):
+        return forms.Media(js=["bundle-codemirror.js"])
+
+    def __init__(self, attrs=None,
+                 *,
+                 language_mode=None, interaction_mode,
+                 indent_unit: int,
+                 autofocus: bool,
+                 additional_keys: dict[str, JsLiteral],
+                 **kwargs):
+        super().__init__(attrs, **kwargs)
+        self.language_mode = language_mode
+        self.interaction_mode = interaction_mode
+        self.indent_unit = indent_unit
+        self.autofocus = autofocus
+        self.additional_keys = additional_keys
+
+    # TODO: Maybe add VSCode keymap?
+    # https://github.com/replit/codemirror-vscode-keymap
+    def render(self, name, value, attrs=None, renderer=None):
+        # based on
+        # https://github.com/codemirror/basic-setup/blob/b3be7cd30496ee578005bd11b1fa6a8b21fcbece/src/codemirror.ts
+        extensions = [
+                JsLiteral(f"rlCodemirror.indentUnit.of({' ' * self.indent_unit !r})"),
+                ]
+
+        if self.interaction_mode == "vim":
+            extensions.insert(0, JsLiteral("rlCodemirror.vim()"))
+        elif self.interaction_mode == "emacs":
+            extensions.insert(0, JsLiteral("rlCodemirror.emacs()"))
+        else:
+            pass
+
+        if self.language_mode is not None:
+            extensions.append(JsLiteral(f"rlCodemirror.{self.language_mode}()"))
+
+        additional_keys = [
+            {
+                "key": key,
+                "run": func,
+            }
+            for key, func in self.additional_keys.items()
+        ]
+        output = [super().render(
+                        name, value, attrs, renderer),
+                  f"""
+                  <script type="text/javascript">
+                    rlCodemirror.editorFromTextArea(
+                        document.getElementById('id_{name}'),
+                        {repr_js(extensions)},
+                        {repr_js(self.autofocus)},
+                        {repr_js(additional_keys)}
+                        )
+                  </script>
+                  """]
+
+        return "\n".join(output)
+
+
 def get_codemirror_widget(
         language_mode: str,
         interaction_mode: str | None,
         config: dict | None = None,
-        addon_css: tuple = (),
-        addon_js: tuple = (),
-        dependencies: tuple = (),
-        read_only: bool = False,
         autofocus: bool = False,
-        additional_keys: dict[str, str | CodeMirrorJavascript] | None = None,
-        attrs: dict[str, str] | None = None,
+        additional_keys: dict[str, JsLiteral] | None = None,
         ) -> tuple[CodeMirrorTextarea, str]:
-    from codemirror import CodeMirrorJavascript, CodeMirrorTextarea
     if additional_keys is None:
         additional_keys = {}
 
-    theme = "default"
-    if read_only:
-        theme += " relate-readonly"
-
     from django.urls import reverse
-    help_text = (_("Press F9 to toggle full-screen mode. ")
+    help_text = (_("Press Esc then Tab to leave the editor. ")
             + _("Set editor mode in <a href='%s'>user profile</a>.")
             % reverse("relate-user_profile"))
 
-    actual_addon_css = ("dialog/dialog", "display/fullscreen", *addon_css)
-    actual_addon_js = (
-        "search/searchcursor",
-        "dialog/dialog",
-        "search/search",
-        "comment/comment",
-        "edit/matchbrackets",
-        "display/fullscreen",
-        "selection/active-line",
-        "edit/trailingspace",
-        *addon_js)
-
-    if language_mode == "python":
+    if language_mode in ["python", "yaml"]:
         indent_unit = 4
     else:
         indent_unit = 2
 
-    extra_keys = {
-            "Ctrl-/": "toggleComment",
-            "Tab": CodeMirrorJavascript("""function(cm)
-                  {
-                    // from https://github.com/codemirror/CodeMirror/issues/988
-
-                    if (cm.doc.somethingSelected()) {
-                        return CodeMirror.Pass;
-                    }
-                    var spacesPerTab = cm.getOption("indentUnit");
-                    var spacesToInsert = (
-                        spacesPerTab
-                        - (cm.doc.getCursor("start").ch % spacesPerTab));
-                    var spaces = Array(spacesToInsert + 1).join(" ");
-                    cm.replaceSelection(spaces, "end", "+input");
-                  }"""),
-            "Shift-Tab": "indentLess",
-            "F9": CodeMirrorJavascript("""function(cm) {
-                      cm.setOption("fullScreen",
-                        !cm.getOption("fullScreen"));
-                  }"""),
-            }
-    extra_keys.update(additional_keys)
-
-    actual_config = {
-            "fixedGutter": True,
-            "matchBrackets": True,
-            "styleActiveLine": True,
-            "showTrailingSpace": True,
-            "indentUnit": indent_unit,
-            "readOnly": read_only,
-            "extraKeys": extra_keys,
-            }
-
-    if autofocus:
-        actual_config["autofocus"] = True
-
-    if interaction_mode == "vim":
-        actual_config["vimMode"] = True
-        actual_addon_js += ("../keymap/vim",)
-    elif interaction_mode == "emacs":
-        actual_config["keyMap"] = "emacs"
-        actual_addon_js += ("../keymap/emacs",)
-    elif interaction_mode == "sublime":
-        actual_config["keyMap"] = "sublime"
-        actual_addon_js += ("../keymap/sublime",)
-    # every other interaction mode goes to default
-
-    if config is not None:
-        actual_config.update(config)
-
-    if attrs is None:
-        attrs = {}
-
     return CodeMirrorTextarea(
-                    mode=language_mode,
-                    dependencies=dependencies,
-                    theme=theme,
-                    addon_css=actual_addon_css,
-                    addon_js=actual_addon_js,
-                    config=actual_config,
-                    attrs=attrs), help_text
+                    language_mode=language_mode,
+                    interaction_mode=interaction_mode,
+                    indent_unit=indent_unit,
+                    autofocus=autofocus,
+                    additional_keys=additional_keys,
+                    ), help_text
 
 # }}}
 

relate/static/js/codemirror.js
@@ -1,0 +1,161 @@
+import { Compartment, EditorState } from '@codemirror/state';
+import {
+  EditorView, keymap, lineNumbers, highlightActiveLine, highlightActiveLineGutter,
+  drawSelection, rectangularSelection, dropCursor, highlightSpecialChars,
+  highlightTrailingWhitespace,
+} from '@codemirror/view';
+import {
+  defaultKeymap, history, historyKeymap, indentWithTab,
+} from '@codemirror/commands';
+import { searchKeymap, highlightSelectionMatches } from '@codemirror/search';
+import {
+  HighlightStyle,
+  syntaxHighlighting, indentOnInput, bracketMatching,
+  foldGutter, foldKeymap, indentUnit,
+} from '@codemirror/language';
+import {
+  autocompletion, completionKeymap,
+  closeBrackets, closeBracketsKeymap,
+} from '@codemirror/autocomplete';
+import { python } from '@codemirror/lang-python';
+import { markdown } from '@codemirror/lang-markdown';
+import { yaml } from '@codemirror/lang-yaml';
+
+import { tags } from '@lezer/highlight';
+
+import { vim, Vim } from '@replit/codemirror-vim';
+import { emacs } from '@replit/codemirror-emacs';
+
+let anyEditorChangedFlag = false;
+
+export function anyEditorChanged() {
+  return anyEditorChangedFlag;
+}
+
+const myListener = new Compartment();
+
+const rlDefaultKeymap = [
+  ...closeBracketsKeymap,
+  ...defaultKeymap,
+  ...searchKeymap,
+  ...historyKeymap,
+  ...completionKeymap,
+  ...foldKeymap,
+  indentWithTab,
+];
+
+// Based on https://discuss.codemirror.net/t/dynamic-light-mode-dark-mode-how/4709/5
+// Use a class highlight style, so we can handle things in CSS.
+const highlightStyle = HighlightStyle.define([
+  { tag: tags.atom, class: 'cmt-atom' },
+  { tag: tags.comment, class: 'cmt-comment' },
+  { tag: tags.keyword, class: 'cmt-keyword' },
+  { tag: tags.literal, class: 'cmt-literal' },
+  { tag: tags.number, class: 'cmt-number' },
+  { tag: tags.operator, class: 'cmt-operator' },
+  { tag: tags.separator, class: 'cmt-separator' },
+  { tag: tags.string, class: 'cmt-string' },
+]);
+
+const defaultExtensionsBase = [
+  lineNumbers(),
+  history(),
+  foldGutter(),
+  indentOnInput(),
+  drawSelection(),
+  EditorState.allowMultipleSelections.of(true),
+  dropCursor(),
+  syntaxHighlighting(highlightStyle, { fallback: true }),
+  bracketMatching(),
+  closeBrackets(),
+  autocompletion(),
+  rectangularSelection(),
+  highlightActiveLine(),
+  highlightActiveLineGutter(),
+  highlightSelectionMatches(),
+  highlightSpecialChars(),
+  highlightTrailingWhitespace(),
+];
+
+// based on https://codemirror.net/docs/migration/
+export function editorFromTextArea(textarea, extensions, autofocus, additionalKeys) {
+  // vim/emacs must come before other extensions
+  extensions.push(
+    ...defaultExtensionsBase,
+    keymap.of([
+      ...rlDefaultKeymap,
+      ...additionalKeys,
+    ]),
+    EditorView.updateListener.of((viewUpdate) => {
+      if (viewUpdate.docChanged) {
+        anyEditorChangedFlag = true;
+      }
+    }),
+    myListener.of(EditorView.updateListener.of(
+      () => { },
+    )),
+  );
+
+  if (textarea.disabled) {
+    extensions.push(
+      EditorState.readOnly.of(true),
+      EditorView.editable.of(false),
+    );
+  }
+
+  const view = new EditorView({ doc: textarea.value, extensions });
+
+  textarea.parentNode.insertBefore(view.dom, textarea);
+  // eslint-disable-next-line no-param-reassign
+  textarea.style.display = 'none';
+  if (textarea.form) {
+    textarea.form.addEventListener('submit', () => {
+      // eslint-disable-next-line no-param-reassign
+      textarea.value = view.state.doc.toString();
+    });
+  }
+  textarea.classList.add('rl-managed-by-codemirror');
+
+  if (autofocus) {
+    document.addEventListener('DOMContentLoaded', () => {
+      view.focus();
+    });
+  }
+
+  return view;
+}
+
+export function setListener(view, fn) {
+  view.dispatch({
+    effects: myListener.reconfigure(
+      EditorView.updateListener.of(fn),
+    ),
+  });
+}
+
+Vim.defineEx('write', 'w', () => {
+  const textarea = document.querySelector('textarea.rl-managed-by-codemirror');
+  if (textarea.form) {
+    const { form } = textarea;
+
+    // prefer 'submit' over 'save' on flow pages
+    let submitButton = form.querySelector("input[type='submit'][name='submit']");
+    if (submitButton) {
+      submitButton.click();
+      return;
+    }
+
+    submitButton = form.querySelector("input[type='submit']");
+    if (submitButton) {
+      submitButton.click();
+    }
+  }
+});
+
+export {
+  EditorState,
+  EditorView,
+  indentUnit,
+  vim, emacs,
+  python, markdown, yaml,
+};

relate/utils.py
@@ -55,18 +55,6 @@ class StyledForm(forms.Form):
         self.helper.label_class = "col-lg-2"
         self.helper.field_class = "col-lg-8"
 
-    def style_codemirror_widget(self):
-        from codemirror import CodeMirrorTextarea
-        from crispy_forms.layout import Div
-
-        if self.helper.layout is None:
-            from crispy_forms.helper import FormHelper
-            self.helper = FormHelper(self)
-            self._configure_helper()
-
-        self.helper.filter_by_widget(CodeMirrorTextarea).wrap(
-                Div, css_class="relate-codemirror-container")
-
 
 class StyledModelForm(forms.ModelForm):
     def __init__(self, *args, **kwargs) -> None:

accounts/migrations/0014_alter_user_editor_mode.py
@@ -1,0 +1,24 @@
+# Generated by Django 4.2.14 on 2024-07-24 19:16
+
+from django.db import migrations, models
+
+
+def switch_sublime_to_default(apps, schema_editor):
+    User = apps.get_model("accounts", "User")
+    User.objects.filter(editor_mode="sublime").update(editor_mode="default")
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('accounts', '0013_alter_user_email_alter_user_username'),
+    ]
+
+    operations = [
+        migrations.RunPython(switch_sublime_to_default),
+        migrations.AlterField(
+            model_name='user',
+            name='editor_mode',
+            field=models.CharField(choices=[('default', 'Default'), ('emacs', 'Emacs'), ('vim', 'Vim')], default='default', help_text="Which key bindings you prefer when editing larger amounts of text or code. (If you do not understand what this means, leave it as 'Default'.)", max_length=20, verbose_name='Editor mode'),
+        ),
+    ]

accounts/models.py
@@ -113,7 +113,6 @@ class User(AbstractBaseUser, PermissionsMixin):
                         "leave it as 'Default'.)"),
             choices=(
                 ("default", _("Default")),
-                ("sublime", "Sublime text"),
                 ("emacs", "Emacs"),
                 ("vim", "Vim"),
                 ),

course/exam.py
@@ -302,8 +302,7 @@ class BatchIssueTicketsForm(StyledForm):
 
         from course.utils import get_codemirror_widget
         cm_widget, _cm_help_text = get_codemirror_widget(
-                language_mode={"name": "markdown", "xml": True},
-                dependencies=("xml",),
+                language_mode="markdown",
                 interaction_mode=editor_mode)
 
         help_text = (gettext('Enter <a href="http://documen.tician.de/'
@@ -373,8 +372,6 @@ class BatchIssueTicketsForm(StyledForm):
                     "If non-empty, this code will be used for all exam tickets"),
                 required=False,
                 widget=forms.PasswordInput())
-
-        self.style_codemirror_widget()
 
         self.helper.add_input(
                 Submit(

commit 2ebf73c02d40122e9fd2d98902e03b630e08b945

Fix readonly logic for text/code questions
Broken in https://github.com/inducer/relate/commit/ea36dc0dab3e2ed568f403c6af4e97fde7b0f363, part of gh-864.

The gist is that that change reinterpreted read-only as disabled, which
is not true. In django, disabled fields only provide their initial
value, not the bound value. And the DOM will not include them in form
submissions.

course/page/code.py
@@ -164,12 +164,13 @@ class CodeForm(StyledForm):
                 autofocus=(
                     not read_only
                     and (data is not None and "answer" in data)))
+        if read_only:
+            cm_widget.attrs["readonly"] = None
 
         self.fields["answer"] = forms.CharField(required=True,
             initial=initial_code,
             help_text=cm_help_text,
             widget=cm_widget,
-            disabled=read_only,
             label=_("Answer"))
 
     def clean(self):

course/page/text.py
@@ -52,39 +52,39 @@ class TextAnswerForm(StyledForm):
     use_required_attribute = False
 
     @staticmethod
-    def get_text_widget(widget_type, check_only=False,
+    def get_text_widget(widget_type, read_only=False, check_only=False,
             interaction_mode=None, initial_text=None):
         """Returns None if no widget found."""
 
+        help_text = None
         if widget_type in [None, "text_input"]:
             if check_only:
                 return True
 
             widget = forms.TextInput()
-            widget.attrs["autofocus"] = None
-            return widget, None
 
         elif widget_type == "textarea":
             if check_only:
                 return True
 
             widget = forms.Textarea()
-            widget.attrs["autofocus"] = None
-            return widget, None
 
         elif widget_type.startswith("editor:"):
             if check_only:
                 return True
 
             from course.utils import get_codemirror_widget
-            cm_widget, cm_help_text = get_codemirror_widget(
+            widget, help_text = get_codemirror_widget(
                     language_mode=widget_type[widget_type.find(":")+1:],
                     interaction_mode=interaction_mode)
 
-            return cm_widget, cm_help_text
-
         else:
             return None, None
+
+        widget.attrs["autofocus"] = None
+        if read_only:
+            widget.attrs["readonly"] = None
+        return widget, help_text
 
     def __init__(self, read_only, interaction_mode, validators, *args, **kwargs):
         widget_type = kwargs.pop("widget_type", "text_input")
@@ -92,13 +92,12 @@ class TextAnswerForm(StyledForm):
 
         super().__init__(*args, **kwargs)
         widget, help_text = self.get_text_widget(
-                    widget_type,
+                    widget_type, read_only,
                     interaction_mode=interaction_mode)
         self.validators = validators
         self.fields["answer"] = forms.CharField(
                 required=True,
                 initial=initial_text,
-                disabled=read_only,
                 widget=widget,
                 help_text=help_text,
                 label=_("Answer"))

course/utils.py
@@ -979,7 +979,7 @@ class CodeMirrorTextarea(forms.Textarea):
 def get_codemirror_widget(
         language_mode: str,
         interaction_mode: str | None,
-        config: dict | None = None,
+        *,
         autofocus: bool = False,
         additional_keys: dict[str, JsLiteral] | None = None,
         ) -> tuple[CodeMirrorTextarea, str]:

relate/static/js/codemirror.js
@@ -100,7 +100,7 @@ export function editorFromTextArea(textarea, extensions, autofocus, additionalKe
     )),
   );
 
-  if (textarea.disabled) {
+  if (textarea.disabled || textarea.readOnly) {
     extensions.push(
       EditorState.readOnly.of(true),
       EditorView.editable.of(false),