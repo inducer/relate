466428685eec706ef40b8da3c0b14d1496dc5f17
Andreas Kloeckner <inform@tiker.net>
2025-09-25 17:14:50 -0500

    More types in analytics

--- course/analytics.py
+++ course/analytics.py
@@ -23,27 +23,41 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
 
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, Any, ClassVar, final
 
 from django import http
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ObjectDoesNotExist, PermissionDenied
 from django.db import connection
-from django.shortcuts import get_object_or_404, redirect, render  # noqa
 from django.urls import reverse
 from django.utils.translation import gettext as _, pgettext
+from pytools import not_none
 
 from course.constants import FlowPermission, ParticipationPermission as PPerm
 from course.content import FlowDesc, get_flow_desc
 from course.models import FlowPageVisit, FlowSession
-from course.utils import PageInstanceCache, course_view, render_course_page
+from course.utils import (
+    CoursePageContext,
+    PageInstanceCache,
+    course_view,
+    render_course_page,
+)
+
+
+if TYPE_CHECKING:
+    from collections.abc import Callable
+
+    from course.page.base import PageBase
+    from course.utils import CoursePageContext
 
 
 # {{{ flow list
 
 @login_required
 @course_view
-def flow_list(pctx):
+def flow_list(pctx: CoursePageContext):
     if not pctx.has_permission(PPerm.view_analytics):
         raise PermissionDenied(_("may not view analytics"))
 
@@ -63,19 +77,27 @@ def flow_list(pctx):
 
 # {{{ histogram tool
 
+@dataclass(frozen=True)
 class BinInfo:
-    def __init__(self, title, raw_weight, percentage, url=None):
-        self.title = title
-        self.raw_weight = raw_weight
-        self.percentage = percentage
-        self.url = url
+    title: str
+    raw_weight: float
+    percentage: float | None
+    url: str | None = None
 
 
+@final
 class Histogram:
-    def __init__(self, num_bin_count=10, num_bin_starts=None,
-            num_min_value=None, num_max_value=None,
-            num_enforce_bounds=False, num_log_bins=False,
-            num_bin_title_formatter=str):
+    num_values: list[tuple[float, float]]
+    string_weights: dict[str, float]
+
+    def __init__(self,
+                num_bin_count: int = 10,
+                num_bin_starts: list[float] | None = None,
+                num_min_value: float | None = None,
+                num_max_value: float | None = None,
+                num_enforce_bounds: bool = False,
+                num_log_bins: bool = False,
+                num_bin_title_formatter: Callable[[Any], str] = str):
         self.string_weights = {}
         self.num_values = []
         self.num_bin_starts = num_bin_starts
@@ -85,7 +107,7 @@ class Histogram:
         self.num_log_bins = num_log_bins
         self.num_bin_title_formatter = num_bin_title_formatter
 
-    def add_data_point(self, value, weight=1):
+    def add_data_point(self, value: float | str | None, weight: float = 1):
         if isinstance(value, str):
             self.string_weights[value] = \
                     self.string_weights.get(value, 0) + weight
@@ -157,7 +179,7 @@ class Histogram:
                         min_value+bin_width*i
                         for i in range(self.num_bin_count)]
 
-        bins = [0 for i in range(len(num_bin_starts))]
+        bins: list[float] = [0 for _i in range(len(num_bin_starts))]
 
         temp_string_weights = self.string_weights.copy()
 
@@ -249,7 +271,7 @@ def is_page_multiple_submit(flow_desc: FlowDesc, page_desc):
 
 # {{{ flow analytics
 
-def make_grade_histogram(pctx, flow_id):
+def make_grade_histogram(pctx: CoursePageContext, flow_id: str):
     qset = FlowSession.objects.filter(
             course=pctx.course,
             flow_id=flow_id,
@@ -266,39 +288,61 @@ def make_grade_histogram(pctx, flow_id):
                         pgettext("Status of session", "in progress"),
                         ">"]))
         else:
-            hist.add_data_point(session.points_percentage())
+            pperc = session.points_percentage()
+            if pperc is not None:
+                pperc = float(pperc)
+
+            hist.add_data_point(pperc)
 
     return hist
 
 
+@dataclass
 class PageAnswerStats:
-    def __init__(self, group_id, page_id, title, average_correctness,
-            average_emptiness, answer_count, total_count, url=None):
-        self.group_id = group_id
-        self.page_id = page_id
-        self.title = title
-        self.average_correctness_percent = 99.99*average_correctness
-        self.average_emptiness_percent = 99.99*average_emptiness
-        self.average_wrongness_percent = 99.99*(
-                1-average_correctness-average_emptiness)
-        self.answer_count = answer_count
-        self.total_count = total_count
-        self.url = url
+    group_id: str
+    page_id: str
+    title: str
+    average_correctness: float
+    average_emptiness: float
+    answer_count: int
+    total_count: int
+    url: str | None
+
+    @property
+    def average_wrongness(self):
+        return 1-self.average_correctness-self.average_emptiness
+
+    _ui_scale_factor: ClassVar[float] = 99.99
+
+    @property
+    def average_correctness_ui(self):
+        return self.average_correctness * self._ui_scale_factor
+
+    @property
+    def average_emptiness_ui(self):
+        return self.average_emptiness * self._ui_scale_factor
+
+    @property
+    def average_wrongness_ui(self):
+        return self.average_wrongness * self._ui_scale_factor
 
 
-def safe_div(num, denom):
+def safe_div(num: float, denom: float):
     if denom == 0:
         return 0
     return num/denom
 
 
-def make_page_answer_stats_list(pctx, flow_id, restrict_to_first_attempt):
+def make_page_answer_stats_list(
+            pctx: CoursePageContext,
+            flow_id: str,
+            restrict_to_first_attempt: bool):
     flow_desc = get_flow_desc(pctx.repo, pctx.course, flow_id,
             pctx.course_commit_sha)
 
     page_cache = PageInstanceCache(pctx.repo, pctx.course, flow_id)
 
-    page_info_list = []
+    page_info_list: list[PageAnswerStats] = []
     for group_desc in flow_desc.groups:
         for page_desc in group_desc.pages:
             points = 0
@@ -377,7 +421,7 @@ def make_page_answer_stats_list(pctx, flow_id, restrict_to_first_attempt):
                     PageAnswerStats(
                         group_id=group_desc.id,
                         page_id=page_desc.id,
-                        title=title,
+                        title=not_none(title),
                         average_correctness=safe_div(points, graded_count),
                         average_emptiness=safe_div(
                             empty_count, graded_count),
@@ -395,7 +439,7 @@ def make_page_answer_stats_list(pctx, flow_id, restrict_to_first_attempt):
     return page_info_list
 
 
-def make_time_histogram(pctx, flow_id):
+def make_time_histogram(pctx: CoursePageContext, flow_id: str):
     qset = FlowSession.objects.filter(
             course=pctx.course,
             flow_id=flow_id)
@@ -415,6 +459,7 @@ def make_time_histogram(pctx, flow_id):
                         pgettext("Status of session", "in progress"),
                         ">"]))
         else:
+            assert session.completion_time is not None
             delta = session.completion_time - session.start_time
             minutes = delta.total_seconds() / 60
             hist.add_data_point(minutes)
@@ -422,7 +467,7 @@ def make_time_histogram(pctx, flow_id):
     return hist
 
 
-def count_participants(pctx, flow_id):
+def count_participants(pctx: CoursePageContext, flow_id: str):
     if not connection.features.can_distinct_on_fields:
         return None
 
@@ -437,11 +482,11 @@ def count_participants(pctx, flow_id):
 
 @login_required
 @course_view
-def flow_analytics(pctx, flow_id):
+def flow_analytics(pctx: CoursePageContext, flow_id: str):
     if not pctx.has_permission(PPerm.view_analytics):
         raise PermissionDenied(_("may not view analytics"))
 
-    restrict_to_first_attempt = int(
+    restrict_to_first_attempt = bool(
             bool(pctx.request.GET.get("restrict_to_first_attempt") == "1"))
 
     try:
@@ -468,18 +513,17 @@ def flow_analytics(pctx, flow_id):
 
 # {{{ page analytics
 
+@dataclass(frozen=True)
 class AnswerStats:
-    def __init__(self, normalized_answer, correctness, count,
-            percentage):
-        self.normalized_answer = normalized_answer
-        self.correctness = correctness
-        self.count = count
-        self.percentage = percentage
+    normalized_answer: str
+    correctness: float | None
+    count: int
+    percentage: float
 
 
 @login_required
 @course_view
-def page_analytics(pctx, flow_id, group_id, page_id):
+def page_analytics(pctx: CoursePageContext, flow_id: str, group_id: str, page_id: str):
     if not pctx.has_permission(PPerm.view_analytics):
         raise PermissionDenied(_("may not view analytics"))
 
@@ -519,7 +563,7 @@ def page_analytics(pctx, flow_id, group_id, page_id):
             .select_related("flow_session")
             .select_related("page_data"))
 
-    normalized_answer_and_correctness_to_count = {}
+    normalized_answer_and_correctness_to_count: dict[tuple[str, float | None], int] = {}
 
     title = None
     body = None
@@ -558,7 +602,7 @@ def page_analytics(pctx, flow_id, group_id, page_id):
 
         total_count += 1
 
-    answer_stats = []
+    answer_stats: list[AnswerStats] = []
     for (normalized_answer, correctness), count in \
             normalized_answer_and_correctness_to_count.items():
         answer_stats.append(
