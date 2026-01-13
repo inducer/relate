361a8d28a61ce8898816af1524590ae60b8ab413
Andreas Kloeckner <inform@tiker.net>
2026-01-13 13:50:39 -0600

    Switch to granian for deployment

--- relate/wsgi.py
+++ relate/wsgi.py
@@ -10,10 +10,12 @@ from __future__ import annotations
 
 import os
 
+from granian.utils.proxies import wrap_wsgi_with_proxy_headers
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "relate.settings")
 
 from django.core.wsgi import get_wsgi_application
 
 
-application = get_wsgi_application()
+application = wrap_wsgi_with_proxy_headers(get_wsgi_application())
