0108a1c2a66f9d363365b07838907b70ea420bde
Andreas Kloeckner <inform@tiker.net>
2026-08-06 14:58:02 +0200

    Migrate to fullcalendar 7

--- frontend/js/fullcalendar.js
+++ frontend/js/fullcalendar.js
@@ -1,12 +1,17 @@
-import { Calendar } from '@fullcalendar/core';
-import allLocales from '@fullcalendar/core/locales-all';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import listPlugin from '@fullcalendar/list';
-import timeGridPlugin from '@fullcalendar/timegrid';
+import { Calendar } from 'fullcalendar';
+import dayGridPlugin from 'fullcalendar/daygrid';
+import listPlugin from 'fullcalendar/list';
+import allLocales from 'fullcalendar/locales-all';
+import themePlugin from 'fullcalendar/themes/classic';
+import timeGridPlugin from 'fullcalendar/timegrid';
+
+import 'fullcalendar/skeleton.css';
+import 'fullcalendar/themes/classic/theme.css';
+import 'fullcalendar/themes/classic/palette.css';
 
 export function setupCalendar(domEl, events, initialDate, locale) {
   const calendar = new Calendar(domEl, {
-    plugins: [dayGridPlugin, timeGridPlugin, listPlugin],
+    plugins: [dayGridPlugin, timeGridPlugin, listPlugin, themePlugin],
     locales: allLocales,
     initialView: 'dayGridMonth',
     headerToolbar: {
